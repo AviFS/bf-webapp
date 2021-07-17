a7eb83936a9dd2a4405d7b4ad45378860d9e3dee
AviFS <avifsternlieb@gmail.com>
2021-07-17 01:16:28 -0700

    Added basic styling

--- interp.js
+++ interp.js
@@ -1,3 +1,18 @@
+function logWithHTML(tape, ptr) {
+    var asterisk = true;
+    var num =  5;
+    var debug = '';
+    var curr;
+    for (var i = 0; i < num; i++) {
+        curr = tape[i].toString().padStart(3, ' ');
+        if (tape[i] == 0) { curr = "<span class='zero'>"+curr+"</span>"; }
+        if (i == ptr) { curr = "<span class='ptr'>"+curr+(asterisk?'*':' ')+"</span>"; }
+        else { curr += ' '; }
+        debug += curr + '    ';
+    }
+    return debug + '\n';
+}
+
 function log(tape, ptr) {
     var debug = '';
     var curr;
@@ -11,7 +26,7 @@ function log(tape, ptr) {
 }
 
 function interp(code, input) {
-    var tape = new Array(9).fill(0); 
+    var tape = new Array(30000).fill(0); 
     var ptr = 0; var inputIndex = 0;
     var output = ''; var debug = '';
 
@@ -25,7 +40,7 @@ function interp(code, input) {
         if (c == ".") { bf2js += "output += String.fromCodePoint(tape[ptr]);"; }
         if (c == "[") { bf2js += "while (tape[ptr]) {"; }
         if (c == "]") { bf2js += "}"; }
-        if (c == "#") { bf2js += "debug += log(tape, ptr);"; }
+        if (c == "#") { bf2js += "debug += logWithHTML(tape, ptr);"; }
         bf2js += "tape[ptr]=(tape[ptr]+256)%256;";
     }
     bf2js += "return [output, debug];} compile();"
